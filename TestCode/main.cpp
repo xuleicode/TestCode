92cfa29fce185d6ab6d4571090cdfb8658281fcd
xulei <xulei163@163.com>
2020-06-07 14:23:40 +0800

    init

---
+++ TestCode/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+#include <string>
+
+int main()
+{
+	cout << "===test begin===" << endl;
+
+	std::string strIn;
+	cin >> strIn;
+
+	cout << strIn << endl;
+
+
+
+
+
+
+
+	cout << "===test end====" << endl;
+	system("pause");
+}
